3c91182fcf562740bb882eb1aeb2159012e8214c
vrvrbie <salikhova1103@mail.ru>
2026-04-13 20:57:04 +0300

    Add files via upload

---
+++ Module4/ShoeStoreApp/MessageHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace ShoeStoreApp.Helpers
+{
+    public static class MessageHelper
+    {
+        public static void ShowInfo(string message)
+        {
+            MessageBox.Show(message, "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static void ShowError(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        public static void ShowWarning(string message)
+        {
+            MessageBox.Show(message, "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        public static DialogResult ShowQuestion(string message)
+        {
+            return MessageBox.Show(message, "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+        }
+    }
+}
